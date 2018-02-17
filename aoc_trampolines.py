d416758ea9d104a8b581db84c99a2d9859ea8aa3
mc_vibraphone <tom.mcmanus@gmail.com>
2018-02-16 20:54:14 -0500

    Finsihed off part 2

--- aoc_trampolines.py
+++ aoc_trampolines.py
@@ -13,12 +13,41 @@ def trampolines(input_data):
         curr = nxt
     return count
 
+def trampolines_2(input_data):
+    ''' Take a list of jumps as input, then process until it's out of the list. '''
+    count = 0
+    curr = 0
+    nxt = 0
+    end = len(input_data)
+    max = -1
+    while nxt < end:
+        count += 1
+        if max < curr:
+            max = curr
+            print('highest idx has been [{}]'.format(max))
+        nxt = input_data[curr] + curr
+        if input_data[curr] > 2:
+            input_data[curr] -= 1
+        else:
+            input_data[curr] += 1
+        curr = nxt
+
+    return count
+
+
 
 if __name__ == '__main__':
     print('Hello world again!')
+
     with open('./input_data', 'r') as f:
         _data = f.read()
-
         _data = list(map(int, _data.split()))
+    t1 = trampolines(_data)
+    print('T1 is {}'.format(t1))
 
-    print('Step count is {}'.format(trampolines(_data)))
+    with open('./input_data', 'r') as f:
+        _data = f.read()
+        _data = list(map(int, _data.split()))
+    t2 = trampolines_2(_data)
+    print('T2 is {}'.format(t2))
+
--- test_d5.py
+++ test_d5.py
@@ -1,13 +1,21 @@
 import unittest
-from aoc_trampolines import trampolines
+from aoc_trampolines import trampolines, trampolines_2
 
 results = [{'input':list(map(int, '''0
     3
     0
     1
     -3'''.split())),
-            'output':5},
-          ]
+    'output':5},]
+
+results2 = [{'input':list(map(int,
+    '''0
+        3
+        0
+        1
+        -3'''.split())),
+            'output':10},
+            ]
 
 class TrampolineTest(unittest.TestCase):
     def test_results(self):
@@ -15,6 +23,9 @@ class TrampolineTest(unittest.TestCase):
         for r in results:
             print('Checking \n[{}]\n exits in {} steps'.format(r['input'], r['output']))
             self.assertEqual(r['output'], trampolines(r['input']))
+        for r in results2:
+            print('Checking \n[{}]\n exits in {} steps'.format(r['input'], r['output']))
+            self.assertEqual(r['output'], trampolines_2(r['input']))
 
 if __name__ == '__main__':
     print('hello world')
